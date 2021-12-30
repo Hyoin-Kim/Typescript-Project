81113efd165ffd73000c186bff4c2c78b3ad4d28
Hyoin-kim <hi980506@naver.com>
2021-12-31 02:03:05 +0900

    stlye : 불필요한 코드 정리

--- front-end/src/styles/GlobalStyle.ts
+++ front-end/src/styles/GlobalStyle.ts
@@ -14,17 +14,6 @@ const GlobalStyle = createGlobalStyle`
     line-height: 19.6px;
     font-weight: 400;
   }
-
-  input{
-    border: none;
-    padding: 5px;
-    border-bottom : 1px solid #E6E6E6;
-  }
-
-  input:focus{
-    outline:none;
-    border-bottom : 1px solid #848484;
-  }
 `;
 
 export default GlobalStyle;
