52b302681c8c3e6d0d623945da01a2f5311a0294
Hyoin-kim <hi980506@naver.com>
2021-12-31 02:43:53 +0900

    feat : 주민등록번호 입력 조건 구현중

--- front-end/src/components/molecules/IdentifyInput.tsx
+++ front-end/src/components/molecules/IdentifyInput.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import StyledInput from "../atoms/StyledInput";
 
 const IdentifyInput = () => {
+  const [isConditionMet, setIsConditionMet] = useState({
+    uniqueNum: false,
+  });
+  const [uniqueNum, setUniqueNum] = useState("");
+  const handleNameOnChange = (value: string) => {
+    setUniqueNum(value);
+    console.log(value);
+  };
+
+  useEffect(() => {
+    const nameCheck = /^[0-9]+$/;
+    if (nameCheck.test(uniqueNum)) {
+      setIsConditionMet({ ...isConditionMet, uniqueNum: true });
+    } else {
+      setIsConditionMet({ ...isConditionMet, uniqueNum: false });
+    }
+  }, [uniqueNum]);
+
   return (
     <IdentifyWrapper>
       <p>주민등록번호</p>
-      <input type="text"></input> <div>-</div> <input type="text"></input>
+      <StyledInput
+        width="124px"
+        errorMsg="올바른 주민등록번호를 입력하세요."
+        onChange={handleNameOnChange}
+        isConditionMet={isConditionMet.uniqueNum}
+      />{" "}
+      -{" "}
+      <StyledInput
+        width="124px"
+        errorMsg="올바른 주민등록번호를 입력하세요."
+        onChange={handleNameOnChange}
+        isConditionMet={isConditionMet.uniqueNum}
+      />
     </IdentifyWrapper>
   );
 };
@@ -14,10 +45,6 @@ export default IdentifyInput;
 
 const IdentifyWrapper = styled.div`
   margin: 0px 24px 32px 24px;
-
-  & > input {
-    width: 124px;
-  }
   & > div {
     display: inline-block;
     margin: 15px;
