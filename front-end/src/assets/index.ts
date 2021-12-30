0cc262f1fbb5bbf9d74c72ca15eaaa7b33a6c522
Hyoin-kim <hi980506@naver.com>
2021-12-31 02:01:09 +0900

    chore : 필요한 에셋 export를 위한 index파일 생성

---
+++ front-end/src/assets/index.ts
@@ -0,0 +1,7 @@
+export { default as AllCheckIcon } from "./allCheck_color.svg";
+export { default as AllCheckGrayIcon } from "./allCheck.svg";
+export { default as CheckIcon } from "./check_color.svg";
+export { default as CheckGrayIcon } from "./check.svg";
+export { default as SecurityImg } from "./security.svg";
+export { default as HomeBarIcon } from "./homeBar.svg";
+export { default as BatteryIcon } from "./batteryIcon.svg";
