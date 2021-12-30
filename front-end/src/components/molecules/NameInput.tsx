9bcd17bbf3745c42b183702f9c2ea012df6cfbc6
Hyoin-kim <hi980506@naver.com>
2021-12-31 02:03:42 +0900

    feat : 이름 Input창 조건(이름,영어) 구현

--- front-end/src/components/molecules/NameInput.tsx
+++ front-end/src/components/molecules/NameInput.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import StyledInput from "../atoms/StyledInput";
 
-const NameInput = () => {
+const NameInput = (): React.ReactElement => {
+  const [isConditionMet, setIsConditionMet] = useState({
+    name: false,
+  });
+  const [name, setName] = useState("");
+  const handleNameOnChange = (value: string) => {
+    setName(value);
+    console.log(value);
+  };
+
+  useEffect(() => {
+    const nameCheck = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/;
+    if (nameCheck.test(name)) {
+      setIsConditionMet({ ...isConditionMet, name: true });
+    } else {
+      setIsConditionMet({ ...isConditionMet, name: false });
+    }
+  }, [name]);
+
   return (
     <NameWrapper>
       <p>이름</p>
-      <input type="text"></input>
+      <StyledInput
+        width="300px"
+        errorMsg="한글과 영문만 가능합니다."
+        onChange={handleNameOnChange}
+        isConditionMet={isConditionMet.name}
+      />
     </NameWrapper>
   );
 };
@@ -14,8 +38,4 @@ export default NameInput;
 
 const NameWrapper = styled.div`
   margin: 26px 24px 32px 24px;
-
-  & > input {
-    width: 300px;
-  }
 `;
