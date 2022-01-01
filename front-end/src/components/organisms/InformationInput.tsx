5d8affb18f1b1293b80165a49cae1274552ca06c
Hyoin-Kim <hi980506@naver.com>
2022-01-01 22:55:06 +0900

    feat : 부모 컴포넌트에 props 정리 및 버튼 구현

--- front-end/src/components/organisms/InformationInput.tsx
+++ front-end/src/components/organisms/InformationInput.tsx
@@ -1,16 +1,47 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState, useRef, useEffect } from "react";
 import NameInput from "../molecules/NameInput";
 import PhoneInput from "../molecules/PhoneInput";
 import IdentifyInput from "../molecules/IdentifyInput";
 import Footer from "../common/Footer";
 
-const InformationInput = () => {
+interface IProps {
+  handleOnKeyPress?: (value: string) => void;
+}
+
+const InformationInput = ({ handleOnKeyPress }: IProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isJoinButtonDisabled, setIsJoinButtonDisabled] = useState(false);
+  const [isConditionMet, setIsConditionMet] = useState({
+    name: false,
+    phoneNum: false,
+    uniqueNum: false,
+  });
+
+  useEffect(() => {
+    console.log("current: ", inputRef);
+    if (inputRef.current !== null) inputRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    if (isConditionMet.name && isConditionMet.phoneNum && isConditionMet.uniqueNum) {
+      setIsJoinButtonDisabled(true);
+    } else {
+      setIsJoinButtonDisabled(false);
+    }
+  }, [isConditionMet]);
+
   return (
     <div>
-      <NameInput />
-      <PhoneInput />
-      <IdentifyInput />
-      <Footer />
+      <NameInput
+        inputRef={inputRef}
+        onKeyPress={handleOnKeyPress}
+        isConditionMet={isConditionMet}
+        setIsConditionMet={setIsConditionMet}
+      />
+      <PhoneInput isConditionMet={isConditionMet} setIsConditionMet={setIsConditionMet} />
+      <IdentifyInput isConditionMet={isConditionMet} setIsConditionMet={setIsConditionMet} />
+      <Footer isJoinButtonDisabled={isJoinButtonDisabled} />
     </div>
   );
 };
