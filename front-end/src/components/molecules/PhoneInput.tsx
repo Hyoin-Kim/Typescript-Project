20cf7be33bd914f03a30cfd236bc54d6dc3423f8
Hyoin-Kim <hi980506@naver.com>
2022-01-01 22:54:25 +0900

    feat : 전화번호 입력시 해당하는 조건 구현

--- front-end/src/components/molecules/PhoneInput.tsx
+++ front-end/src/components/molecules/PhoneInput.tsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import StyledInput from "../atoms/StyledInput";
 
-const PhoneInput = () => {
+interface IProps {
+  isConditionMet: {
+    name: boolean;
+    phoneNum: boolean;
+    uniqueNum: boolean;
+  };
+  setIsConditionMet: (value: { name: boolean; phoneNum: boolean; uniqueNum: boolean }) => void;
+}
+
+const PhoneInput = ({ isConditionMet, setIsConditionMet }: IProps) => {
+  const [phoneNum, setPhoneNum] = useState("");
+
+  const handleNameOnChange = (value: any) => {
+    setPhoneNum(value);
+    console.log("숫자", value);
+  };
+
+  useEffect(() => {
+    console.log(phoneNum.length);
+    if (phoneNum.length === 10) {
+      setPhoneNum(phoneNum.replace(/-/g, "").replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3"));
+      setIsConditionMet({ ...isConditionMet, phoneNum: true });
+    } else if (phoneNum.length === 13) {
+      setPhoneNum(phoneNum.replace(/-/g, "").replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3"));
+      setIsConditionMet({ ...isConditionMet, phoneNum: true });
+    } else {
+      setIsConditionMet({ ...isConditionMet, phoneNum: false });
+    }
+  }, [phoneNum]);
+
   return (
     <PhoneWrapper>
       <p>휴대폰 번호</p>
-      <input type="text"></input>
+      <StyledInput
+        width="300px"
+        errorMsg="올바른 휴대폰 번호를 입력하세요."
+        onChange={handleNameOnChange}
+        isConditionMet={isConditionMet.phoneNum}
+        maxByte={13}
+        value={phoneNum}
+      />
     </PhoneWrapper>
   );
 };
@@ -14,8 +51,4 @@ export default PhoneInput;
 
 const PhoneWrapper = styled.div`
   margin: 0px 24px 32px 24px;
-
-  & > input {
-    width: 300px;
-  }
 `;
