cdaaf55ccf45ddeb69d80ba806b40beb3046bb13
Hyoin-Kim <hi980506@naver.com>
2022-01-02 20:16:20 +0900

    feat : 주민등록번호 개편 전 조건 구현

--- front-end/src/components/molecules/IdentifyInput.tsx
+++ front-end/src/components/molecules/IdentifyInput.tsx
@@ -1,34 +1,77 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import StyledInput from "../atoms/StyledInput";
+import { IJoinInfo } from "../../types/info.type";
 
 interface IProps {
   birthInputRef?: React.RefObject<HTMLInputElement>;
   uniqueInputRef?: React.RefObject<HTMLInputElement>;
   onKeyPress: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-  isConditionMet: {
-    name: boolean;
-    phoneNum: boolean;
-    uniqueNum: boolean;
-  };
-  setIsConditionMet: (value: { name: boolean; phoneNum: boolean; uniqueNum: boolean }) => void;
+  isConditionMet: IJoinInfo;
+  setIsConditionMet: (value: IJoinInfo) => void;
 }
 
 const IdentifyInput = ({ birthInputRef, uniqueInputRef, onKeyPress, isConditionMet, setIsConditionMet }: IProps) => {
   const maxByteRef = useRef<HTMLInputElement>(null);
+  const [birthNum, setBirthNum] = useState("");
   const [uniqueNum, setUniqueNum] = useState("");
-  const handleNameOnChange = (value: string) => {
+  const handleBirthOnChange = (value: string) => {
+    setBirthNum(value);
+  };
+
+  const handleUniqueOnChange = (value: string) => {
     setUniqueNum(value);
   };
 
-  if (uniqueNum.length === 6) {
+  if (birthNum.length === 6) {
     maxByteRef.current?.focus();
   }
 
   useEffect(() => {
     const nameCheck = /^[0-9]+$/;
+    if (nameCheck.test(birthNum)) {
+      setIsConditionMet({ ...isConditionMet, birthNum: true });
+    } else {
+      setIsConditionMet({ ...isConditionMet, birthNum: false });
+    }
+  }, [birthNum]);
+
+  useEffect(() => {
+    const nameCheck = /^[0-9]+$/;
     if (nameCheck.test(uniqueNum)) {
-      setIsConditionMet({ ...isConditionMet, uniqueNum: true });
+      const genderNum = uniqueNum.slice(0, 1);
+      if (genderNum >= "1" && genderNum <= "4") {
+        const checkArr = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5];
+        const birthArr = birthNum.split("").map(function (item) {
+          return parseInt(item);
+        });
+        const uniqueArr = uniqueNum.split("").map(function (item) {
+          return parseInt(item);
+        });
+        const combineArr = birthArr.concat(uniqueArr);
+        console.log(combineArr);
+        for (let i = 0; i < combineArr.length - 1; i++) {
+          combineArr[i] = combineArr[i] * checkArr[i];
+        }
+        const uniqueLastNum = combineArr[combineArr.length - 1];
+        let sum = 0;
+        for (let i = 0; i < combineArr.length - 1; i++) {
+          sum += combineArr[i];
+        }
+        sum = sum % 11;
+        sum = 11 - sum;
+        if (sum > 9) {
+          sum = sum % 10;
+        }
+        if (sum === uniqueLastNum) {
+          setIsConditionMet({ ...isConditionMet, uniqueNum: true });
+        } else {
+          setIsConditionMet({ ...isConditionMet, uniqueNum: false });
+        }
+      } else {
+        setIsConditionMet({ ...isConditionMet, uniqueNum: false });
+      }
     } else {
       setIsConditionMet({ ...isConditionMet, uniqueNum: false });
     }
@@ -40,8 +83,8 @@ const IdentifyInput = ({ birthInputRef, uniqueInputRef, onKeyPress, isConditionM
       <StyledInput
         width="124px"
         errorMsg="올바른 주민등록번호를 입력하세요."
-        onChange={handleNameOnChange}
-        isConditionMet={isConditionMet.uniqueNum}
+        onChange={handleBirthOnChange}
+        isConditionMet={isConditionMet.birthNum}
         maxByte={6}
         onKeyPress={onKeyPress}
         inputRef={birthInputRef}
@@ -50,7 +93,8 @@ const IdentifyInput = ({ birthInputRef, uniqueInputRef, onKeyPress, isConditionM
       <StyledInput
         width="124px"
         errorMsg="올바른 주민등록번호를 입력하세요."
-        onChange={handleNameOnChange}
+        onChange={handleUniqueOnChange}
+        isPw={true}
         isConditionMet={isConditionMet.uniqueNum}
         maxByte={7}
         inputRef={uniqueInputRef || maxByteRef}
