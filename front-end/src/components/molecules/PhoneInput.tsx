fd5a65eded425a35f156c7e1d2a47ca8c203c8a4
Hyoin-Kim <hi980506@naver.com>
2022-01-02 20:15:21 +0900

    refactor : 변수명 수정 및 코드 깔끔하게 정리

--- front-end/src/components/molecules/PhoneInput.tsx
+++ front-end/src/components/molecules/PhoneInput.tsx
@@ -1,39 +1,32 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import StyledInput from "../atoms/StyledInput";
+import { IJoinInfo } from "../../types/info.type";
 
 interface IProps {
   phoneInputRef?: React.RefObject<HTMLInputElement>;
   onKeyPress: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-  isConditionMet: {
-    name: boolean;
-    phoneNum: boolean;
-    uniqueNum: boolean;
-  };
-  setIsConditionMet: (value: { name: boolean; phoneNum: boolean; uniqueNum: boolean }) => void;
+  isConditionMet: IJoinInfo;
+  setIsConditionMet: (value: IJoinInfo) => void;
 }
 
 const PhoneInput = ({ phoneInputRef, onKeyPress, isConditionMet, setIsConditionMet }: IProps) => {
   const [phoneNum, setPhoneNum] = useState("");
 
-  const handleNameOnChange = (value: string) => {
+  const handlePhoneOnChange = (value: string) => {
     setPhoneNum(value);
   };
 
   useEffect(() => {
-    if (phoneNum.length >= 3) {
-      if (phoneNum.length === 10) {
-        setPhoneNum(phoneNum.replace(/-/g, "").replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3"));
-      } else if (phoneNum.length === 13) {
-        setPhoneNum(phoneNum.replace(/-/g, "").replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3"));
-      }
-      const oldPhoneNum = /^01([1|9|])-?([0-9]{3,4})-?([0-9]{4})$/;
-      const recentPhoneNum = /^01([0])-?([0-9]{4})-?([0-9]{4})$/;
-      if (oldPhoneNum.test(phoneNum) === true || recentPhoneNum.test(phoneNum)) {
-        setIsConditionMet({ ...isConditionMet, phoneNum: true });
-      } else {
-        setIsConditionMet({ ...isConditionMet, phoneNum: false });
-      }
+    if (phoneNum.length === 10) {
+      setPhoneNum(phoneNum.replace(/-/g, "").replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3"));
+    } else if (phoneNum.length === 13) {
+      setPhoneNum(phoneNum.replace(/-/g, "").replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3"));
+    }
+    const oldPhoneNum = /^01([1|9|])-?([0-9]{3,4})-?([0-9]{4})$/;
+    const recentPhoneNum = /^01([0])-?([0-9]{4})-?([0-9]{4})$/;
+    if (oldPhoneNum.test(phoneNum) === true || recentPhoneNum.test(phoneNum)) {
+      setIsConditionMet({ ...isConditionMet, phoneNum: true });
     } else {
       setIsConditionMet({ ...isConditionMet, phoneNum: false });
     }
@@ -45,7 +38,7 @@ const PhoneInput = ({ phoneInputRef, onKeyPress, isConditionMet, setIsConditionM
       <StyledInput
         width="300px"
         errorMsg="올바른 휴대폰 번호를 입력하세요."
-        onChange={handleNameOnChange}
+        onChange={handlePhoneOnChange}
         isConditionMet={isConditionMet.phoneNum}
         maxByte={13}
         value={phoneNum}
