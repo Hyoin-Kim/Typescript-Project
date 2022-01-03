8a4d6b1573791b88a6ebd640a178689713776b21
Hyoin-Kim <hi980506@naver.com>
2022-01-03 15:31:22 +0900

    chore : 아토믹디자인패턴의 Template 생성

---
+++ front-end/src/components/templates/UserInfoTemplate.tsx
@@ -0,0 +1,13 @@
+import InformationInput from "../organisms/InformationInput";
+import Poster from "../molecules/Poster";
+
+const UserInfoTemplate = () => {
+  return (
+    <div>
+      <Poster />
+      <InformationInput />
+    </div>
+  );
+};
+
+export default UserInfoTemplate;
